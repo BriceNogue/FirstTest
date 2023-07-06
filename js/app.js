0d7bacc74269e2f1d2c7e0ee2e06ca437324ad60
BriceNogue <bricenogue@gmail.com>
2023-07-06 09:56:00 +0200

    First Commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+new Vue({
+    el:'#app',
+    data: {
+        message: 'Bonjour Vue',
+        link: 'http://grafikart.fr',
+        succes: false,
+        persons: [
+            {
+                firstname:'Alpha',
+                lastname: 'Brice'
+            },
+            {
+                firstname:'Nogue',
+                lastname: 'Junior'
+            },
+            {
+                firstname:'Wafo',
+                lastname: 'Idriss'
+            },
+        ],
+    }
+})
